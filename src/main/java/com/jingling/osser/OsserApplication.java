f7ae17d42804cfbf70342be2716d71fc6ad7d5b7
unknown <xujingling927@gmail.com>
2023-01-16 09:14:28 +0800

    Initial commit

---
+++ src/main/java/com/jingling/osser/OsserApplication.java
@@ -0,0 +1,13 @@
+package com.jingling.osser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jingling/osser/OsserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingling.osser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
